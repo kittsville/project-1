01edc56a2a3e0b1a73841e549507141f106bd865
Christopher Stewart <stewpot64@gmail.com>
2015-01-28 15:04:58 +0000

    Fixed missing inheritance, typo and  self.rect in Player class.

--- lib/player.py
+++ lib/player.py
@@ -1,17 +1,19 @@
 import pygame
 
 # Handles player position etc.
-class Player:
+class Player(pygame.sprite.Sprite):
     # Set movement vector
     change_x = 0
     change_y = 0
 
     # Construct player robot at given location
     def __init__(self, xPos, yPos):
+        # Call the parent class (Sprite) constructor
         pygame.sprite.Sprite.__init__(self)
 
         # Load image and set width, height
-        self.image, self.rect = pygame.image.load("assests/robot.png").convert_alpha()
+        self.image = pygame.image.load("assets/robot.png").convert_alpha()
+        self.rect = self.image.get_rect()
 
         # Make our top-left corner the passed-in location
         self.rect.x = xPos
